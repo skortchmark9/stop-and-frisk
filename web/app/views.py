c96e1ed6a608386f721bf1d2c0a18a1cef9fda5b
skortchmark9 <samuel_kortchmar@brown.edu>
2015-05-05 16:53:08 -0400

    state 2 lat lng & vv

--- web/app/views.py
+++ web/app/views.py
@@ -1,5 +1,14 @@
 from flask import render_template
 from app import app
+import csv
+from pyproj import Proj
+
+#US Survey Feet, NY Long Island
+e_3628 = '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +to_meter=0.3048006096012192 +no_defs'
+
+p = Proj(e_3628)
+to_meter = 0.3048006096012192
+
 
 @app.route('/')
 def main():
@@ -11,4 +20,32 @@ def selection():
 
 @app.route('/heatmap')
 def heatmap():
+    sample = open('Data/2014_sample.csv')
+    reader = csv.DictReader(sample)
+    for row in reader:
+        state_x = float(row['xcoord'])
+        state_y = float(row['ycoord'])
+        lat, lng = state_2_lat_lng(state_x, state_y)
+        print(lat, lng, row['race'])
+        break
+
     return render_template('heatmap.html')
+
+def state_2_lat_lng(state_x, state_y):
+    state_x = state_x * to_meter
+    state_y = state_y * to_meter
+    lng, lat = p(state_x, state_y, inverse=True)
+    return lat, lng
+
+def lat_lng_2_state(lat, lng):
+    m_x, m_y = p(lng, lat)
+    m_x = m_x / to_meter
+    m_y = m_y / to_meter
+    return (m_x, m_y)
+
+def us_ft_2_m(ft):
+    return ft * to_meter
+
+def m_2_us_ft(m):
+    return m / to_meter
+
