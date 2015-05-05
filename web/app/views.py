8f3fec9e5985edd94a4e8c23ccd2ba91698c9d7b
skortchmark9 <samuel_kortchmar@brown.edu>
2015-05-05 17:32:45 -0400

    plotting stops

--- web/app/views.py
+++ web/app/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 from app import app
 import csv
 from pyproj import Proj
+import json
 
 #US Survey Feet, NY Long Island
 e_3628 = '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +to_meter=0.3048006096012192 +no_defs'
@@ -22,26 +23,29 @@ def selection():
 def heatmap():
     sample = open('Data/2014_sample.csv')
     reader = csv.DictReader(sample)
+    data = []
     for row in reader:
-        state_x = float(row['xcoord'])
-        state_y = float(row['ycoord'])
-        lat, lng = state_2_lat_lng(state_x, state_y)
-        print(lat, lng, row['race'])
-        break
+        try:
+            state_x = float(row['xcoord'])
+            state_y = float(row['ycoord'])
+            lat, lng = state_2_lat_lng(state_x, state_y)
+            data.append({'lat' : lat, 'lng' : lng, 'race' : row['race']})
+        except ValueError as e:
+            print('ERROR: ', row['xcoord'], row['ycoord'], row['race'])
 
-    return render_template('heatmap.html')
+    return render_template('heatmap.html', data=json.dumps(data))
 
 def state_2_lat_lng(state_x, state_y):
-    state_x = state_x * to_meter
-    state_y = state_y * to_meter
+    state_x = us_ft_2_m(state_x)
+    state_y = us_ft_2_m(state_y)
     lng, lat = p(state_x, state_y, inverse=True)
     return lat, lng
 
 def lat_lng_2_state(lat, lng):
     m_x, m_y = p(lng, lat)
-    m_x = m_x / to_meter
-    m_y = m_y / to_meter
-    return (m_x, m_y)
+    f_x = m_2_us_ft(m_x)
+    f_y = m_2_us_ft(m_y)
+    return (f_x, f_y)
 
 def us_ft_2_m(ft):
     return ft * to_meter
