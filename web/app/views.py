1e3829ea659efe48e61fbb35c043ead3e2306821
skortchmark9 <samuel_kortchmar@brown.edu>
2015-05-07 20:27:04 -0400

    real-world data on timeline

--- web/app/views.py
+++ web/app/views.py
@@ -3,12 +3,13 @@ from app import app
 import csv
 import json
 from conversion import state_2_lat_lng
-
+from collections import Counter
 
 
 @app.route('/')
 def main():
-    return render_template('layout.html', data=getHeatmapSampleData())
+    data = getSampleData(True, True)
+    return render_template('layout.html', heatmap_data=json.dumps(data['heatmap_data']), timeline_graph_data=json.dumps(data['timeline_graph_data']))
 
 @app.route('/selection')
 def selection():
@@ -16,22 +17,37 @@ def selection():
 
 @app.route('/heatmap')
 def heatmap():
-    return render_template('heatmap.html', data=getHeatmapSampleData())
+    data = getSampleData(True, False)
 
+    return render_template('heatmap.html', heatmap_data=json.dumps(data['heatmap_data']))
 
-def getHeatmapSampleData():
+def getSampleData(heatmap=False, timeline_graph_data=False):
     sample = open('web/app/static/data/2014_sample.csv')
     reader = csv.DictReader(sample)
-    data = []
+    heatmap_data = []
+    timeline_counter = Counter()
+
     for row in reader:
         try:
-            state_x = float(row['xcoord'])
-            state_y = float(row['ycoord'])
-            lat, lng = state_2_lat_lng(state_x, state_y)
-            data.append({'lat' : lat, 'lng' : lng, 'race' : row['race']})
+            if heatmap:
+                state_x = float(row['xcoord'])
+                state_y = float(row['ycoord'])
+                lat, lng = state_2_lat_lng(state_x, state_y)
+                heatmap_data.append({'lat' : lat, 'lng' : lng, 'race' : row['race']})
+
+            if timeline_graph_data:
+                datestop = row['datestop']
+                if len(datestop) == 7:
+                    datestop = '0' + datestop
+
+                timeline_counter[datestop] += 1
+
         except ValueError as e:
             print('ERROR: ', row['xcoord'], row['ycoord'], row['race'])
 
-    return json.dumps(data)
+    total = sum([timeline_counter[k] for k in timeline_counter])
+    timeline_data = [{'date' : k, 'chance' : int(v * 100/ total)} for k, v in timeline_counter.iteritems()]
 
+    data = {'heatmap_data' : heatmap_data, 'timeline_graph_data' : timeline_data}
 
+    return data
