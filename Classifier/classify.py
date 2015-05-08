c0ab71a4654ed887b6d7d384a5745a62594911a4
avanhatt <alexa_van_hattum@brown.edu>
2015-05-07 21:24:46 -0400

    Added the classifier directory, with pickled files and classify.py

---
+++ Classifier/classify.py
@@ -0,0 +1,64 @@
+from __future__ import division
+import sys
+import csv
+import argparse
+import cPickle
+from collections import defaultdict
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+
+def main():
+	
+	test_dict = {'time': '01012013', 'sex': 'M', 'race': 'B', 'age': '17'}
+	print get_probs_all_tracts(test_dict)
+
+
+def get_probs_all_tracts(feature_dict):
+
+ 	with open('saved_classifier.pkl', 'rb') as fid:
+		classifier = cPickle.load(fid)
+	
+	with open('saved_vectorizer.pkl', 'rb') as fid:
+		vectorizer = cPickle.load(fid)
+
+	with open('saved_average.pkl', 'rb') as fid:
+		average = cPickle.load(fid)
+
+	with open('tracts.pkl', 'rb') as fid:
+		tracts = cPickle.load(fid)
+
+	feature_dict['time'] = str_to_time(feature_dict['time'])
+	feature_dict['age'] = (int(feature_dict['age']) - 10)/70.0
+
+	prob_dict = {}
+	for tract in tracts:
+		tract_dict = feature_dict
+		tract_dict['tract'] = tract
+
+		features = vectorizer.transform([tract_dict])
+
+		probs = classifier.predict_proba(features)[0]
+		prob_dict[tract] = probs[1]/average
+	return prob_dict
+
+def str_to_time(s):
+	""" Converts stop and frisk style date strings to time vector in [0, 1]"""
+	print s
+	if '-' in s: # format: 'yyyy-mm-dd' 
+		year = int(s[:4])
+		month = int(s[5:7])
+		date = int(s[8:10])
+
+	else: # format: 'mddyyy' or 'mmddyyyy'
+		l = len(s)
+		year = int(s[(l-4):])
+		month = int(s[:(l-6)])
+		date = int(s[(l-6):(l-4)])
+
+	days = 365*(year-2003) + int(round(365.25/12*(month-1))) + (date-1)
+	time = days/(365.25*11.9918)
+	return time
+
+if __name__ == '__main__':
+	main()
