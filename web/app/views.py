222f4d3057a418162f19be255f04eb0ad8616abd
skortchmark9 <samuel_kortchmar@brown.edu>
2015-05-07 21:12:11 -0400

    set up & removed d3 dependency

--- web/app/views.py
+++ web/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 from app import app
 import csv
 import json
@@ -14,6 +14,17 @@ def main():
 @app.route('/selection')
 def selection():
     return render_template("selection.html" )
+
+@app.route('/alexa', methods=['POST'])
+def alexa():
+    print(request)
+    print(request.form['time'])
+    return jsonify(success=True)
+    print('HIT')
+    # {'time' : 'MMDDYYYY', 'sex' : 'F/M', 'race' : 'W/H/B/O'. 'age' : '++'}
+    # return [{'tract' : 22}]
+
+
 
 @app.route('/heatmap')
 def heatmap():
