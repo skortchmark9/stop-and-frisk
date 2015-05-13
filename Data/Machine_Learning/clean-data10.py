dd7faaad25888b0a7886c610ebd63b6a683b580e
avanhatt <alexa_van_hattum@brown.edu>
2015-05-12 23:50:27 -0400

    added ml scripts

---
+++ Data/Machine_Learning/clean-data10.py
@@ -0,0 +1,34 @@
+import sys
+import csv
+
+
+def main():
+	args = sys.argv
+	if len(args) != 4:
+		print 'Useage: clean-stop-and-frisk.py <data filename>\
+		 <output filename> <attribute list filename>'
+		return
+	csv_reader = csv.reader(open(args[1], 'rU'))
+	csv_writer = csv.writer(open(args[2], 'w'))
+	attributes = [int(line)-1 for line in open(args[3]).readlines()]
+
+	for line in csv_reader:
+		row = []
+		skip = False
+		for a in attributes:
+			if line[a] == ' ':
+				skip = True
+				break
+			if a == 83 and line[a] != 'age': # age attribute
+				age = line[a]
+				if age == '**' or int(age) < 10 or int(age) > 94:
+					skip = True
+					break
+			row.append(line[a])
+		if skip == True: 
+			continue
+		csv_writer.writerow(row)
+
+
+if __name__ == '__main__':
+	main()
